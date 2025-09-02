5879a753b82b1d168a34b3d701786e187ce63758
BrutaleKnut <Elev600258@elev.trondelagfylke.no>
2025-09-02 13:30:41 +0200

    Fullførte oppgave 2

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ if (name != null) {
     console.log('Hei' + ' ' + name)
 }
 
+let number = prompt('Skriv et tall', 1)
+
+let tall = prompt('Det tallet suger, skriv et bedre tall', 72)
+if (isNaN(number), isNaN(tall)) {
+console.log('Jeg sa tall, dit fjols')
+} else {
+console.log(+tall + +number)
+console.log(number - tall)
+console.log(tall * number)
+console.log(number / tall)  
+}
